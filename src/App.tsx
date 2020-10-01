04ea28b2cbf100d85643fff8d2d60525e85d642d
Joey <pothofj@hva.nl>
2020-10-01 13:36:17 +0200

    Created create funeral page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import theme from './theme.js';
+import { ThemeProvider } from 'emotion-theming';
+import Header from './components/header/Header';
+import { Flex, Box, Button } from 'rebass';
+import { Label, Input } from '@rebass/forms';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={{
+        colors: {
+          background: 'black',
+          primary: 'tomato',
+        },
+        space: [0, 6, 12, 24, 48],
+        fontSizes: [14, 16, 18, 20, 24],
+        radii: {
+          default: 12,
+        }
+      }}>
+      <Header />
+      <Box
+        as='form'
+        onSubmit={e => e.preventDefault()}
+        p={4}
+        mx='auto'
+        width={[1, 1, 1 / 3]}
+        bg='#C4C4C4'>
+        <Flex>
+          <Box 
+            width={1} 
+            p={3}
+            mb={4}
+            bg='#A6A6A6'>
+            Funeral 1
+          </Box>
+        </Flex>
+        <Flex>
+          <Box 
+            width={1} 
+            p={3}
+            mb={4}
+            bg='#A6A6A6'>
+            Funeral 2
+          </Box>
+        </Flex>
+        <Flex>
+          <Box 
+            width={1} 
+            p={3}
+            mb={4}
+            bg='#A6A6A6'>
+            Funeral 3
+          </Box>
+        </Flex>
+        <Flex>
+          <Box 
+            px={2}
+            mx='auto'>
+            <Button
+              bg='#A6A6A6'
+              color='black'>
+              Create new funeral
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+    </ThemeProvider>
   );
 }
 
